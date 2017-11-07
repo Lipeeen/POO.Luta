bb18dc9767213c5e3866de74b6b3684020b4b223
Luiz <luizi@DESKTOP-REK0LJU>
2017-11-07 15:46:39 -0200

    Commit inicial

---
+++ Luta/Luta/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Luta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_mago = new System.Windows.Forms.Button();
+            this.btn_lutadordesumo = new System.Windows.Forms.Button();
+            this.btn_soldado = new System.Windows.Forms.Button();
+            this.btn_general = new System.Windows.Forms.Button();
+            this.btn_dragao = new System.Windows.Forms.Button();
+            this.btn_ninja = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_mago
+            // 
+            this.btn_mago.BackColor = System.Drawing.Color.Lime;
+            this.btn_mago.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btn_mago.ForeColor = System.Drawing.Color.Blue;
+            this.btn_mago.Location = new System.Drawing.Point(440, 353);
+            this.btn_mago.Name = "btn_mago";
+            this.btn_mago.Size = new System.Drawing.Size(75, 45);
+            this.btn_mago.TabIndex = 4;
+            this.btn_mago.Text = "Mago";
+            this.btn_mago.UseVisualStyleBackColor = false;
+            // 
+            // btn_lutadordesumo
+            // 
+            this.btn_lutadordesumo.BackColor = System.Drawing.Color.Lime;
+            this.btn_lutadordesumo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btn_lutadordesumo.ForeColor = System.Drawing.Color.Blue;
+            this.btn_lutadordesumo.Location = new System.Drawing.Point(132, 353);
+            this.btn_lutadordesumo.Name = "btn_lutadordesumo";
+            this.btn_lutadordesumo.Size = new System.Drawing.Size(75, 45);
+            this.btn_lutadordesumo.TabIndex = 5;
+            this.btn_lutadordesumo.Text = "Lutador de sumô";
+            this.btn_lutadordesumo.UseVisualStyleBackColor = false;
+            // 
+            // btn_soldado
+            // 
+            this.btn_soldado.BackColor = System.Drawing.Color.Lime;
+            this.btn_soldado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btn_soldado.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.btn_soldado.Location = new System.Drawing.Point(440, 83);
+            this.btn_soldado.Name = "btn_soldado";
+            this.btn_soldado.Size = new System.Drawing.Size(75, 45);
+            this.btn_soldado.TabIndex = 6;
+            this.btn_soldado.Text = "Soldado";
+            this.btn_soldado.UseVisualStyleBackColor = false;
+            // 
+            // btn_general
+            // 
+            this.btn_general.BackColor = System.Drawing.Color.Lime;
+            this.btn_general.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_general.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btn_general.ForeColor = System.Drawing.Color.Blue;
+            this.btn_general.Location = new System.Drawing.Point(132, 83);
+            this.btn_general.Name = "btn_general";
+            this.btn_general.Size = new System.Drawing.Size(75, 45);
+            this.btn_general.TabIndex = 7;
+            this.btn_general.Text = "General";
+            this.btn_general.UseVisualStyleBackColor = false;
+            // 
+            // btn_dragao
+            // 
+            this.btn_dragao.BackColor = System.Drawing.Color.Lime;
+            this.btn_dragao.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btn_dragao.ForeColor = System.Drawing.Color.Blue;
+            this.btn_dragao.Location = new System.Drawing.Point(759, 353);
+            this.btn_dragao.Name = "btn_dragao";
+            this.btn_dragao.Size = new System.Drawing.Size(75, 45);
+            this.btn_dragao.TabIndex = 8;
+            this.btn_dragao.Text = "Dragão";
+            this.btn_dragao.UseVisualStyleBackColor = false;
+            // 
+            // btn_ninja
+            // 
+            this.btn_ninja.BackColor = System.Drawing.Color.Lime;
+            this.btn_ninja.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btn_ninja.ForeColor = System.Drawing.Color.Blue;
+            this.btn_ninja.Location = new System.Drawing.Point(759, 83);
+            this.btn_ninja.Name = "btn_ninja";
+            this.btn_ninja.Size = new System.Drawing.Size(75, 45);
+            this.btn_ninja.TabIndex = 9;
+            this.btn_ninja.Text = "Ninja";
+            this.btn_ninja.UseVisualStyleBackColor = false;
+            this.btn_ninja.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Lime;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(266, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(393, 29);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Player 1 - Escolha seu personagem";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox6.Image = global::Luta.Properties.Resources.dragao;
+            this.pictureBox6.Location = new System.Drawing.Point(675, 418);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(236, 204);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 22;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox5.Image = global::Luta.Properties.Resources.mago;
+            this.pictureBox5.Location = new System.Drawing.Point(355, 418);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(236, 204);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 21;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox4.Image = global::Luta.Properties.Resources.lutadordesumo;
+            this.pictureBox4.Location = new System.Drawing.Point(50, 418);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(236, 204);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 20;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox3.Image = global::Luta.Properties.Resources.ninja;
+            this.pictureBox3.Location = new System.Drawing.Point(675, 144);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(228, 194);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.Image = global::Luta.Properties.Resources.soldado;
+            this.pictureBox2.Location = new System.Drawing.Point(355, 144);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(236, 194);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::Luta.Properties.Resources.general;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 144);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(236, 194);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Luta.Properties.Resources.fundo;
+            this.ClientSize = new System.Drawing.Size(959, 660);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_ninja);
+            this.Controls.Add(this.btn_dragao);
+            this.Controls.Add(this.btn_general);
+            this.Controls.Add(this.btn_soldado);
+            this.Controls.Add(this.btn_lutadordesumo);
+            this.Controls.Add(this.btn_mago);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_mago;
+        private System.Windows.Forms.Button btn_lutadordesumo;
+        private System.Windows.Forms.Button btn_soldado;
+        private System.Windows.Forms.Button btn_general;
+        private System.Windows.Forms.Button btn_dragao;
+        private System.Windows.Forms.Button btn_ninja;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
+
